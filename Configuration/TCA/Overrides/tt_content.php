c0dd7260c6580d075f0bd43dfab9c2194beefbbc
Dirk Koritnik <koritnik@werkraum-media.de>
2022-09-28 13:49:08 +0200

    Add basic ce setup

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -4,11 +4,60 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') || die();
 
-ExtensionUtility::registerPlugin(
-    'Watchlist',
-    'Watchlist',
-    'LLL:EXT:watchlist/Resources/Private/Language/locallang.xlf:plugin.watchlist',
-    'EXT:watchlist/Resources/Public/Icons/Extension.svg'
-);
+(static function (string $extKey, string $table, string $contentType) {
+    $languagePath = implode(':', [
+        'LLL',
+        'EXT',
+        $extKey . '/Resources/Private/Language/locallang.xlf',
+        $table . '.' . $contentType . '.',
+    ]);
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['watchlist_watchlist'] = 'recursive,select_key,pages';
+    ExtensionUtility::registerPlugin(
+        $extKey,
+        $extKey,
+        $languagePath . 'title',
+        'EXT:' . $extKey . '/Resources/Public/Icons/Extension.svg'
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+        $table,
+        'CType',
+        [
+            $languagePath . 'title',
+            $contentType,
+            'content-bullets',
+            'special',
+        ]
+    );
+
+    \TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule($GLOBALS['TCA'][$table], [
+        'ctrl' => [
+            'typeicon_classes' => [
+                $contentType => 'content-bullets',
+            ],
+        ],
+        'types' => [
+            $contentType => [
+                'showitem' => implode(',', [
+                    '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general',
+                        '--palette--;;general',
+                        '--palette--;;headers',
+                    '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance',
+                        '--palette--;;frames',
+                        '--palette--;;appearanceLinks',
+                    '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language',
+                        '--palette--;;language',
+                    '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access',
+                        '--palette--;;hidden',
+                        '--palette--;;access',
+                    '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories',
+                    '--div--;LLL:EXT:core/Resources/Private/Language/locallang_tca.xlf:sys_category.tabs.category',
+                        'categories',
+                    '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes',
+                        'rowDescription',
+                    '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended',
+                ]),
+            ],
+        ],
+    ]);
+})('watchlist', 'tt_content', 'watchlist_watchlist');
